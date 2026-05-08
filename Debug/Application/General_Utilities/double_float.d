5f7ec80a87afeaca8d7150254defb9a6b4c6a0b6
STM-Project <51048656+STM-Project@users.noreply.github.com>
2026-05-08 12:38:05 +0200

    Delete Debug directory

--- Debug/Application/General_Utilities/double_float.d
+++
@@ -1,4 +0,0 @@
-Application/General_Utilities/double_float.o: \
- ../Application/General_Utilities/double_float.c \
- ../Application/General_Utilities/double_float.h
-../Application/General_Utilities/double_float.h:
--- Debug/Application/General_Utilities/mini_printf.d
+++
@@ -1,4 +0,0 @@
-Application/General_Utilities/mini_printf.o: \
- ../Application/General_Utilities/mini_printf.c \
- ../Application/General_Utilities/mini_printf.h
-../Application/General_Utilities/mini_printf.h:
