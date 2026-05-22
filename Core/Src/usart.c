15d5d0a49edf2c70d59f91d823967eef5bcede40
STM-Project <51048656+STM-Project@users.noreply.github.com>
2026-05-22 15:58:00 +0200

    Update usart.c

--- Core/Src/usart.c
+++ Core/Src/usart.c
@@ -46,7 +46,7 @@ void MX_UART7_Init(void)
 
   /* USER CODE END UART7_Init 1 */
   huart7.Instance = UART7;
-  huart7.Init.BaudRate = 2000000;
+  huart7.Init.BaudRate = 115200;
   huart7.Init.WordLength = UART_WORDLENGTH_8B;
   huart7.Init.StopBits = UART_STOPBITS_1;
   huart7.Init.Parity = UART_PARITY_NONE;
@@ -456,12 +456,12 @@ void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
 {
 	if(huart->Instance==UART7)
 	{
-		Dbg(1,"\r\n!! UART7 ERROR !!");
+		Dbg(1,"\r\n!! UART7_ERROR !!");
 	}
 	else if(huart->Instance==USART6)
 	{
 		//WIFI_UartErrorService();
-		Dbg(1,"\r\n!! UART6 ERROR !!");
+		Dbg(1,"\r\n!! UART6_ERROR !!");
 	}
 }
 
@@ -474,7 +474,12 @@ void DEBUG_Send(char *txt){
 void DEBUG_SendDma(uint8_t *txt, int size){
  /* SCB_CleanDCache_by_Addr((uint32_t*)dbgSendBuffer, SEND_BUFF_SIZE); */
     SCB_CleanDCache_by_Addr((uint32_t*)txt, CACHE_ALLIGN_LEN(size));
-    HAL_UART_Transmit_DMA(&huart7, txt, size);
+
+    int result = HAL_UART_Transmit_DMA(&huart7, txt, size);
+    if(result!=HAL_OK)
+    {
+    	asm("nop");
+    }
 }
 
 void DEBUG_ReceiveStart(uint8_t* buffer, uint16_t len)
