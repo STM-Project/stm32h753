e6fae7221b68c809574f42eeee01ed7ac8fcfbd4
STM-Project <51048656+STM-Project@users.noreply.github.com>
2026-08-14 15:53:13 +0200

    Update wwwPages.h

--- Application/wwwPages.h
+++ Application/wwwPages.h
@@ -14,12 +14,19 @@
 #define HTTP_BUFFER_SIZE 		262144
 #define HTTP_BUFFER_SIZE_PRE 	262144
 
+typedef enum{
+	TEMPL_None,
+	TEMPL_Temp,
+	TEMPL_TempRhu,
+}HTTP_TEMPALTE_TYPE;
+
 extern char HttpRefr[];
 extern const char HttpStyle[];
 extern const char HttpMainReadPanel[];
 extern const char HttpMainReadPanel2[];
 extern const char HttpMainSettings[];
 
-DATA_TO_SEND* GetPageWWW(char *getHttpRequest);
+void HTTP_TEMPLATE_TempRhu(char* buff, u8 type,u8 nr, float val1,float val2, char* name1,char* name2);
+
 
 #endif /* HTTP_WWWPAGES_H_ */
